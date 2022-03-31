5e08827e857d74529d3819088e445faf99810df8
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-03-31 18:44:24 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -16,10 +16,14 @@ const object = [
 const form = document.getElementById("form");
 const elInput = document.getElementById("input");
 const result = document.getElementById("result");
+const Positive = document.getElementById("positive");
+const elBtn = document.querySelectorAll(".text");
 
 form.addEventListener("submit", (e)=>{
+  textone.textContent = "";
+  texttwo.textContent = "";
+  textthree.textContent = "";
   e.preventDefault();
-  const elBtn = document.querySelectorAll(".text");
   object.forEach((item, index) =>{
     if(item.id == elInput.value){
       elBtn[index].classList.add("active");
@@ -28,6 +32,35 @@ form.addEventListener("submit", (e)=>{
     }else {
       elBtn[index].classList.remove("active");
     }
-  })
+    if(elInput.value > 3){
+      alert("Wrong number");
+    }
+  });
   elInput.value = "";
-})
+});
+
+let textone = document.createElement('span');
+let texttwo = document.createElement('span');
+let textthree = document.createElement('span');
+let box = document.getElementById('box');
+textone.textContent = "Positive";
+texttwo.textContent = "Negative";
+textthree.textContent = "Neutral";
+
+
+elBtn[0].addEventListener("click", () =>{
+  box.appendChild(textone);
+  texttwo.remove();
+  textthree.remove();
+});
+elBtn[1].addEventListener("click", () =>{
+  box.appendChild(texttwo);
+  textone.remove();
+  textthree.remove();
+});
+
+elBtn[2].addEventListener("click", () =>{
+  box.appendChild(textthree);
+  texttwo.remove();
+  textone.remove();
+});  
